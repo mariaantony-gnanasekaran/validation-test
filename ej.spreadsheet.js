bb9bb4091ac1d2450e5cbd714a29e3ca6e4b5606
mariaantony-gnanasekaran <mariaantony.gnanasekaran@syncfusion.com>
2017-03-16 17:34:29 +0530

    1

---
+++ ej.spreadsheet.js
@@ -0,0 +1,9 @@
+
+(function (fn) {
+    typeof define === 'function' && define.amd ? define(["jsrender","jquery-validation","jquery-validation-unobtrusive"], fn) : fn();
+})
+(function () {
+	
+console.log("fine");
+
+});
